5dda8f17a7e443619d7849031ff44bc6db717761
DaisukeJigen <112531950+DaisukeJigen@users.noreply.github.com>
2022-09-07 22:42:42 -0400

    try again

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link v-if="!loggedIn" to="/login">Login</router-link>
+      <template v-else>
+        <router-link to="/verbs">Verbs</router-link>
+      </template>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { mapState } from "vuex";
+
+@Component({
+  computed: {
+    ...mapState("userData", ["userData"]),
+  },
+})
+export default class App extends Vue {
+  get loggedIn() {
+    const self: any = this;
+    return self.userData.username != undefined;
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+import store from "@/store/index";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+  },
+  {
+    path: "/verbs",
+    name: "verbs",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Verbs.vue"),
+    beforeEnter: (to, from, next) => {
+      const test = store.getters;
+      if (!store.getters["userData/getLoggedIn"]) {
+        next("/login");
+      } else {
+        next();
+      }
+    },
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
