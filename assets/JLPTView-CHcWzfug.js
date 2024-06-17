bc0645b47fa2a9d5918428366fada655d2dddcdd
DaisukeJigen <112531950+DaisukeJigen@users.noreply.github.com>
2024-06-17 13:33:40 -0400

    get rid of about page

---
+++ assets/JLPTView-CHcWzfug.js
@@ -0,0 +1,2 @@
+import{d as o,o as n,i as t,f as r,g as s}from"./index-C6kS6ze5.js";const i=o({__name:"JLPTView",setup(a){return n(()=>{}),(c,_)=>{const e=r("RouterView");return s(),t(e)}}});export{i as default};
+//# sourceMappingURL=JLPTView-CHcWzfug.js.map
