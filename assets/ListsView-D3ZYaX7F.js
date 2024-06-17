5f0caf839f8b21757658a42bef7a4940f5bf2d81
DaisukeJigen <112531950+DaisukeJigen@users.noreply.github.com>
2024-06-17 09:54:13 -0400

    replace wanikanjigation with japanesefuntime

---
+++ assets/ListsView-D3ZYaX7F.js
@@ -0,0 +1,1 @@
+import{d as o,b as t,o as n,i as s,g as r}from"./index-DhkJPS5q.js";const i=o({__name:"ListsView",setup(a){return t(()=>{}),(c,_)=>{const e=r("RouterView");return n(),s(e)}}});export{i as default};
