9d2bcac57f7ae1cda3a7bcdd40b1a4ef7992e9c3
DaisukeJigen <112531950+DaisukeJigen@users.noreply.github.com>
2024-09-02 13:46:04 -0400

    small update

--- assets/WaniKaniLevelView-B_CgiROO.js
+++
@@ -1,2 +0,0 @@
-import{d as t,o as n,v as o,u as a,H as s}from"./index-DZ0TLiOB.js";const _=t({__name:"WaniKaniLevelView",setup(r){const e=s();return n(()=>{}),(u,i)=>"hello "+o(a(e).params.level.toString())}});export{_ as default};
-//# sourceMappingURL=WaniKaniLevelView-B_CgiROO.js.map
--- assets/WaniKaniView-Mbk9jmFU.js
+++
@@ -1,2 +0,0 @@
-import{d as o,o as n,m as t,h as a,i as r}from"./index-DZ0TLiOB.js";const p=o({__name:"WaniKaniView",setup(s){return n(()=>{}),(c,_)=>{const e=a("RouterView");return r(),t(e)}}});export{p as default};
-//# sourceMappingURL=WaniKaniView-Mbk9jmFU.js.map
